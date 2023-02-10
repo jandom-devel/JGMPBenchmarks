512128c18123d0ed113aa0d9f99d399e94bb5431
Gianluca Amato <gianluca.amato.74@gmail.com>
2023-02-10 16:32:21 +0100

    Reformat source code.

--- src/main/java/it/unich/jgmpbenchmarks/JGMPFactorialBenchmark.java
+++ src/main/java/it/unich/jgmpbenchmarks/JGMPFactorialBenchmark.java
@@ -1,39 +1,37 @@
 /**
-* Copyright 2022, 2023 Francesca Scozzari <francesca.scozzari@unich.it> and
-*                      Gianluca Amato <gianluca.amato@unich.it>
-*
-* JGMPBenchmarks is a set of benchmarks for JGMP. JGMPBenchmarks is free software: you can
-* redistribute it and/or modify it under the terms of the GNU General Public
-* License as published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* JGMPBenchmarks is distributed in the hope that it will be useful, but WITHOUT ANY
-* WARRANTY; without even the implied warranty of a MERCHANTABILITY or FITNESS
-* FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-* details.
-*
-* You should have received a copy of the GNU General Public License along with
-* JGMPBenchmarks. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright 2022, 2023 Francesca Scozzari <francesca.scozzari@unich.it> and Gianluca Amato
+ * <gianluca.amato@unich.it>
+ *
+ * JGMPBenchmarks is a set of benchmarks for JGMP. JGMPBenchmarks is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * JGMPBenchmarks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of a MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with JGMPBenchmarks. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
 package it.unich.jgmpbenchmarks;
-import java.math.BigInteger;
 
+import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
+
+import org.jscience.mathematics.number.LargeInteger;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import org.jscience.mathematics.number.LargeInteger;
-
-import java.util.concurrent.TimeUnit;
-
-import it.unich.jgmp.*;
+import it.unich.jgmp.MPZ;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
-@Fork(value = 1, jvmArgs = {"-Xms2G", "-Xmx2G"})
+@Fork(value = 1, jvmArgs = { "-Xms2G", "-Xmx2G" })
 @Warmup(iterations = 1)
 @Measurement(iterations = 1)
 
@@ -67,19 +65,18 @@ public class JGMPFactorialBenchmark {
 
     /**
      * Benchmarks for JGMP using factorial
+     *
      * @param args not used.
      */
     public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-            .include(JGMPFactorialBenchmark.class.getSimpleName())
-            .build();
+        Options opt = new OptionsBuilder().include(JGMPFactorialBenchmark.class.getSimpleName()).build();
 
         new Runner(opt).run();
     }
 
     public static MPZ factorialMPZ(int x) {
         MPZ f = new MPZ(1);
-        while (x >= 1)  {
+        while (x >= 1) {
             f.mulAssign(f, x);
             x -= 1;
         }
