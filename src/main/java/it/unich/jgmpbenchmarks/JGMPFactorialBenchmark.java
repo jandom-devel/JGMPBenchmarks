a0ab857f53826d176eba62a0406ebc8b277d2269
Gianluca Amato <gianluca.amato.74@gmail.com>
2023-02-10 13:31:20 +0100

    Add LargeInteger benchmark for factorial.

--- src/main/java/it/unich/jgmpbenchmarks/JGMPFactorialBenchmark.java
+++ src/main/java/it/unich/jgmpbenchmarks/JGMPFactorialBenchmark.java
@@ -24,6 +24,8 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import org.jscience.mathematics.number.LargeInteger;
+
 import java.util.concurrent.TimeUnit;
 
 import it.unich.jgmp.*;
@@ -42,7 +44,7 @@ public class JGMPFactorialBenchmark {
 
     @Param({ "1000", "10000", "100000" })
     public int fact ;
-    
+
     @Benchmark
     public MPZ factorialMPZ() {
         return factorialMPZ(fact);
@@ -53,17 +55,23 @@ public class JGMPFactorialBenchmark {
         return factorialBigInteger(fact);
     }
 
+    @Benchmark
+    public LargeInteger factorialLargeInteger() {
+        return factorialLargeInteger(fact);
+    }
+
     /**
      * Benchmarks for JGMP using factorial
      * @param args not used.
      */
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
-        .build();
+            .include(JGMPFactorialBenchmark.class.getSimpleName())
+            .build();
 
         new Runner(opt).run();
     }
-    
+
     public static MPZ factorialMPZ(int x) {
         MPZ f = new MPZ(1);
         while (x >= 1)  {
@@ -77,6 +85,15 @@ public class JGMPFactorialBenchmark {
         BigInteger f = BigInteger.ONE;
         while (x >= 1) {
             f = f.multiply(BigInteger.valueOf(x));
+            x -= 1;
+        }
+        return f;
+    }
+
+    public static LargeInteger factorialLargeInteger(int x) {
+        LargeInteger f = LargeInteger.ONE;
+        while (x >= 1) {
+            f = f.times(x);
             x -= 1;
         }
         return f;
