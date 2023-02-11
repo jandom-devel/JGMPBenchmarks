aa74e674c2471ced33a65100868292c980eaa2ff
Francesca Scozzari <fscozzari@unich.it>
2023-02-11 21:49:26 +0100

    Add Euler benchmark and a factorial example.

---
+++ src/main/java/it/unich/jgmpbenchmarks/JGMPFactorialExample.java
@@ -0,0 +1,45 @@
+/**
+* Copyright 2022 Francesca Scozzari <francesca.scozzari@unich.it> and
+*                Gianluca Amato <gianluca.amato@unich.it>
+*
+* JGMPBenchmarks is a set of benchmarks for JGMP. JGMPBenchmarks is free software: you can
+* redistribute it and/or modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* JGMPBenchmarks is distributed in the hope that it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of a MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License along with
+* JGMPBenchmarks. If not, see <http://www.gnu.org/licenses/>.
+*/
+package it.unich.jgmpbenchmarks;
+
+import it.unich.jgmp.*;
+
+/**
+ * Example for JGMP using factorial
+ */
+public class JGMPFactorialExample {
+    
+    /**
+     * Example for JGMP using factorial
+     * @param args not used.
+     */
+    public static void main(String[] args) {
+        int x = 100000;
+        MPZ factorial = factorialMPZ(x);
+        System.out.print(factorial);
+    }
+    
+    public static MPZ factorialMPZ(int x) {
+        MPZ f = new MPZ(1);
+        while (x >= 1)  {
+            f.mulAssign(f, x);
+            x -= 1;
+        }
+        return f;
+    }
+}
