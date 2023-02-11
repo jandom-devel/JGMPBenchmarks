ac3bb18ca97e0dde5c051fde582106dfe8fa5684
Gianluca Amato <gianluca.amato.74@gmail.com>
2023-02-11 18:15:46 +0100

    Add floating point benchmarks.

---
+++ src/main/java/it/unich/jgmpbenchmarks/ContinuedFractionFloatBenchmark.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright 2022, 2023 Francesca Scozzari <francesca.scozzari@unich.it> and Gianluca Amato
+ * <gianluca.amato@unich.it>
+ *
+ * JGMPBenchmarks is a set of benchmarks for JGMP. JGMPBenchmarks is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * JGMPBenchmarks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of a MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with JGMPBenchmarks. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+package it.unich.jgmpbenchmarks;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.numbers.fraction.BigFraction;
+import org.apfloat.Apfloat;
+import org.apfloat.Apint;
+import org.apfloat.Aprational;
+import org.jscience.mathematics.number.FloatingPoint;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import it.unich.jgmp.MPF;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 1, jvmArgs = { "-Xms2G", "-Xmx2G" })
+@Warmup(iterations = 1)
+@Measurement(iterations = 1)
+
+public class ContinuedFractionFloatBenchmark {
+
+    @Param({ "1", "10", "100", "1000" })
+    public int steps;
+
+    @Param({ "1024" })
+    public int prec;
+
+    @Benchmark
+    public BigDecimal continuedFractionBigDecimal() {
+        return continuedFractionBigDecimal(steps, prec);
+    }
+
+    @Benchmark
+    public MPF continuedFractionMPF() {
+        return continuedFractionMPF(steps, prec);
+    }
+
+    @Benchmark
+    public FloatingPoint continuedFractionFloatingPoint() {
+        return continuedFractionFloatingPoint(steps, prec);
+    }
+
+    @Benchmark
+    public Apfloat continuedFractionApfloat() {
+        return continuedFractionApfloat(steps, prec);
+    }
+
+    /**
+     * Benchmarks for JGMP using continued fractions for approximating pi.
+     */
+    public static void main(String[] args) throws RunnerException {
+        String resBigDecimal = "3.141592410971980674262588860216726437293650906745376695673194907396554410588564312078";
+        if (! continuedFractionBigDecimal(100, 128).toString().equals(resBigDecimal))
+            throw new Error("Invalid MPF result");
+        String resMPF = "3.141592410971980674262588860216726437293";
+        if (! continuedFractionMPF(100, 128).toString().equals(resMPF))
+            throw new Error("Invalid MPF result");
+        String resFloatingPoint = "0.314159241097198067426258886021672643729365E1";
+        if (! continuedFractionFloatingPoint(100, 128).toString().equals(resFloatingPoint))
+            throw new Error("Invalid FloatingPoint result");
+        String resApfloat = "3.14159241097198067426258886021672643729365";
+        if (! continuedFractionApfloat(100, 128).toString().equals(resApfloat))
+            throw new Error("Invalid Apfloat result");
+        Options opt = new OptionsBuilder().include(ContinuedFractionFloatBenchmark.class.getSimpleName()).build();
+        new Runner(opt).run();
+    }
+
+
+    /* Bigdecimal */
+    public static BigDecimal continuedFractionBigDecimal(int steps, int prec) {
+        BigDecimal value = BigDecimal.ZERO;
+        BigDecimal six = BigDecimal.valueOf(6);
+        RoundingMode round = RoundingMode.HALF_DOWN;
+        while (steps >= 1) {
+            value = value.add(six);
+            BigDecimal numerator = BigDecimal.valueOf(2 * steps - 1).setScale(prec/3);
+            numerator = numerator.multiply(numerator);
+            value = numerator.divide(value, round);
+            steps -= 1;
+        }
+        value = value.add(BigDecimal.valueOf(3));
+        return value;
+    }
+
+    /* JGMP */
+    public static MPF continuedFractionMPF(int steps, int prec) {
+        MPF value = MPF.init2(prec);
+        MPF numerator = new MPF();
+        MPF six = new MPF(6);
+        while (steps >= 1) {
+            value.addAssign(six);
+            numerator.set(2 * steps - 1);
+            numerator.mulAssign(numerator);
+            value.divAssign(numerator, value);
+            steps -= 1;
+        }
+        value.addAssign(new MPF(3));
+        return value;
+    }
+
+    /* JScience */
+    public static FloatingPoint continuedFractionFloatingPoint(int steps, int prec) {
+        FloatingPoint.setDigits(prec / 3);
+        FloatingPoint value = FloatingPoint.ZERO;
+        FloatingPoint six = FloatingPoint.valueOf(6);
+        while (steps >= 1) {
+            value = value.plus(six);
+            FloatingPoint numerator = FloatingPoint.valueOf(2 * steps - 1);
+            numerator = numerator.times(numerator);
+            value = numerator.divide(value);
+            steps -= 1;
+        }
+        value = value.plus(FloatingPoint.valueOf(3));
+        return value;
+    }
+
+    /* Apfloat */
+    public static Apfloat continuedFractionApfloat(int steps, int prec) {
+        Apfloat value = new Apfloat(0, prec/3);
+        Apfloat six = new Apfloat(6);
+        while (steps >= 1) {
+            value = value.add(six);
+            Apfloat numerator = new Apfloat(2 * steps - 1, prec/3);
+            numerator = numerator.multiply(numerator);
+            value = numerator.divide(value);
+            steps -= 1;
+        }
+        value = value.add(new Apfloat(3));
+        return value;
+    }
+
+}
